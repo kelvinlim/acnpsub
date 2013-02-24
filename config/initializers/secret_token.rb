ca9d89cc65dbc5bec42d90e09dafff45f1ee3916
Kelvin O. Lim <kolim@x-192-168-1-68.tc.umn.edu>
2013-02-24 15:13:35 -0600

    Generated the Rails 3 Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acnpsub::Application.config.secret_token = 'aeaeb7c51b7d74f95b6a4200e0d71a25d6079983de92aeb286f7fbff0718fc6e5c43cf7928af7fa751ace85daccb7fcff04f5f1131c7527cf5d7be6fbebf4f28'
